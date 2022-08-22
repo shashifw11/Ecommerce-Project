7bf3edf6e3fc8887941527411253e8c90d631043
shashifw11 <shashi_fw11_220@masai.school>
2022-08-22 18:22:28 +0530

    update backend

--- src/Components/Account/login.jsx
+++ src/Components/Account/login.jsx
@@ -1,18 +1,21 @@
-import Form from 'react-bootstrap/Form'
+
 import "./login.css"
 import Button from '@material-ui/core/Button';
-import {Link} from "react-router-dom" ; 
+import {Link , useNavigate} from "react-router-dom" ; 
 import {useState,useEffect} from "react"; 
 
 export const Login = ()=>{
 
+  const navigate = useNavigate() ; 
+
   const [users , setUser] = useState({
-        Email : "" , 
-        Password : "" , 
+        email : "" , 
+        password : "" , 
     }) 
 
   const [errors,setErrors] = useState({}) ; 
   const [submit,setSubmit] = useState(false)
+  const [get ,setGet] = useState(false);
 
      
 
@@ -25,12 +28,40 @@ export const Login = ()=>{
     const handleSubmit=()=>{
       setErrors(validate(users))
        setSubmit(true)
+      
+       PostData() ; 
     }
-  
+    
+    const PostData = async()=>{
+        
+      const {email , password} = users 
+       const res =  await fetch("https://backendsasi1.herokuapp.com/login" , {
+       
+         method : "POST" , 
+         headers : {
+          "Content-Type" : "application/json"
+         } , 
+         body : JSON.stringify({email , password})
+       })
 
-     useEffect(()=>{
-           if(Object.keys(errors).length === 0 && submit){
-             console.log(users)
+       const data = await res.json() ;
+         
+         if(res.status === 400 ||  !data ){
+              window.alert("Invalied email or Password") ; 
+                console.log("Invalied Login") 
+                   
+         } else{
+           window.alert("Login Sucessfull");
+           setGet(true);
+           console.log("Sucessful Login");
+          navigate("/");
+         }
+    
+}
+
+     useEffect( ()=>{
+           if(Object.keys(errors).length === 0 && submit== true){
+            //  console.log(users) ; 
            }
 
       },[errors]);
@@ -38,23 +69,23 @@ export const Login = ()=>{
     const validate=(values)=>{
          const errors = {} ; 
          const reges =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-       if(!values.Email){
-         errors.Email = "This is not valid email"
-       }else if(!reges.test(values.Email)){
-        errors.Email = "This is not valid email formate"
+       if(!values.email){
+         errors.email = "This is not valid email"
+       }else if(!reges.test(values.email)){
+        errors.email = "This is not valid email formate"
       }
-       if(!values.Password){
-         errors.Password = "This is not valid password" ; 
-       }else if(values.Password.length < 4){
-        errors.Password = "password must be more than 4 character" ; 
-       }else if(values.Password.length > 8){
-          errors.Password = "password must be less than 8 character" ; 
+       if(!values.password){
+         errors.password = "This is not valid password" ; 
+       }else if(values.password.length < 4){
+        errors.password = "password must be more than 4 character" ; 
+       }else if(values.password.length > 8){
+          errors.password = "password must be less than 8 character" ; 
        }
           return errors  ; 
     } 
 
     return ( <>
-     {Object.keys(errors).length === 0 && submit ? <div style = {{fontSize : "30px" ,  color : "red"}}>Signed in sucessfully</div> : (<h1 className = "h1">Login</h1>)} 
+     {Object.keys(errors).length === 0 && get ? <div style = {{fontSize : "30px" ,  color : "red"}}>Login Successfull</div> : (<h1 className = "h1">Login</h1>)} 
         
         <div className = "container">
           <form onSubmit={(e)=>{
@@ -62,24 +93,28 @@ export const Login = ()=>{
           }}> 
           
               <label>Email </label><br/>
-              <input onChange={handleChange} name = "Email" type = "email" placeholder='Enter email'/><br/>
-              <p>{errors.Email}</p>
+              <input onChange={handleChange} name = "email" type = "email" placeholder='Enter email'/><br/>
+              <p>{errors.email}</p>
               <label>Password</label><br/>
-              <input onChange={handleChange} name = "Password" type = "password" placeholder = "Enter password"/>
-              <p>{ errors.Password}</p>
+              <input onChange={handleChange} name = "password" type = "password" placeholder = "Enter password"/>
+              <p>{ errors.password}</p>
           </form> 
-          <Button onClick = {handleSubmit} style = {{ padding : "10px" , width : "90%" , marginTop : "20px"}} variant="contained" color="primary" disableElevation>
-  Login
-</Button> 
-<p style = {{textAlign : "center" , marginTop : "10px" , fontWeight : "bolder"}}>or</p>
-<Link style = {{textDecoration : "none"}} to = "/signup"><Button  style = {{ padding : "10px"  , width : "90%" ,   marginBottom : "10px"}} variant="contained" color="primary" disableElevation>
-  Rigester
-</Button></Link>
+          <Button onClick = {handleSubmit} 
+          style = {{ padding : "10px" , width : "90%" , marginTop : "25px",   marginRight : "20px",}} variant="contained" color="primary" disableElevation>
+              Login
+            </Button> 
+
+
+         <p style = {{textAlign : "center" , marginTop : "10px" , fontWeight : "bolder"}}>or</p>
+
+       <Link style = {{textDecoration : "none"}} 
+             to = "/signup">
+              <Button  style = {{ padding : "10px"  , width : "90%" , marginRight : "20px" ,   marginBottom : "10px"}} variant="contained" color="primary" disableElevation>
+              Rigester
+           </Button>
+        </Link>
         </div>
        </>
     )
 }
 
-//https://www.youtube.com/watch?v=fWXk2YC8gXI&t=363s
-//https://www.youtube.com/watch?v=lca2MXZnlmw
-//https://www.youtube.com/watch?v=Xc_MPV9EtNs
--- src/Components/Account/signup.jsx
+++ src/Components/Account/signup.jsx
@@ -1,17 +1,20 @@
 import "./signin.css"
 import Button from '@material-ui/core/Button';
-import {Link} from "react-router-dom" ; 
+import {Link , useNavigate} from "react-router-dom" ; 
 import {useState,useEffect} from "react" ; 
 export const SignUP = ()=>{
 
+   const navigate = useNavigate() ; 
+
   const [users , setUsers] = useState({
-         Name : "" , 
-         Email : "" , 
-         Password : "" , 
-         Re_Password : "" , 
+        
+         email : "" , 
+         password : "" , 
+       
   }) 
     const [errors , setErrors] = useState({}) ; 
     const [submit , setSubmit] = useState(false) ; 
+    const [get , setGet] = useState(false);
     
     // console.log(errors)  ; 
 
@@ -24,7 +27,36 @@ export const SignUP = ()=>{
    const handleSubmit = () => {
        setErrors(validate(users))     /// errors update by setErrors and that will recive by validate in return erros
        setSubmit(true)
+       PostData()
       } 
+
+
+      const PostData = async()=>{
+        
+           const {email , password} = users 
+            const res =  await fetch("https://backendsasi1.herokuapp.com/register" , {
+              //"proxy" : "http://localhost:2345", 
+              method : "POST" , 
+              headers : {
+               "Content-Type" : "application/json"
+              } , 
+              body : JSON.stringify({email , password})
+            })
+ 
+            const data = await res.json() ;
+              
+              if(res.status === 400 ||  !data ){
+                       window.alert("User Already Registred") ; 
+                         console.log("Invalied Registration") 
+                        
+              } else{
+                window.alert("Registration Sucessfull");
+                setGet(true);
+                console.log("Sucessful Registration");
+                navigate("/login");
+              }
+         
+     }
 
    useEffect(()=>{
     if(Object.keys(errors).length === 0 && submit){
@@ -37,49 +69,37 @@ export const SignUP = ()=>{
         const errors = {} ; 
         const reges =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ; 
 
-        if(!users.Name){
-          errors.Name = "This is not valid Name"
-        }
-        if(!users.Email){
-          errors.Email = "This is not valid Email"
-        } else if(!reges.test(users.Email)){
-          errors.Email = "Invalid Email formate"
+        if(!users.email){
+          errors.email = "This is not valid Email"
+        } else if(!reges.test(users.email)){
+          errors.email = "Invalid Email formate"
         } 
-        if(!users.Password){
-          errors.Password = "This is not valid Password"
-        } else if(users.Password.length<4){
-          errors.Password = "Password must be more than 4 character"
-        } else if(users.Password.length > 8){
-          errors.Password = "Password must be less than 8 character"
+        if(!users.password){
+          errors.password = "This is not valid Password"
+        } else if(users.password.length<4){
+          errors.password = "Password must be more than 4 character"
+        } else if(users.password.length > 8){
+          errors.password = "Password must be less than 8 character"
         }
-        if(!users.Re_Password){
-          errors.Re_Password = "This is not valid Re-Password"
-        }
-        else if(users.Re_Password != users.Password ){
-          errors.Re_Password = "Re-Password not matched with your password"
-        } 
+        
          return errors
      }
 
     return (
       <>
        
-       { Object.keys(errors).length === 0 && submit ? (<div style = {{fontSize : "30px" ,  color: "#64fe00"}}>Account created successfully</div>) : (<h3 style = {{color: "#1f80e0"}}>Create Account</h3>)}
+       { Object.keys(errors).length === 0 && get ? (<div style = {{fontSize : "30px" ,  color: "#64fe00"}}>Account created successfully</div>) : (<h3 style = {{color: "#1f80e0"}}>Create Account</h3>)}
         <div className="signup">
           
           <form onSubmit = {(e)=>{e.preventDefault()}}>
-            <label>Name</label><br/>
-            <input   onChange = {handleChange} name = "Name" type = "text" placeholder = "Enter Name"/><br/>
-            <p className = "ers">{errors.Name}</p>
+           
             <label>Email</label><br/>
-            <input onChange = {handleChange}  name = "Email" type = "email" placeholder = "Enter email"/><br/>
-            <p className = "ers">{errors.Email}</p>
+            <input onChange = {handleChange}  name = "email" type = "email" placeholder = "Enter email"/><br/>
+            <p className = "ers">{errors.email}</p>
             <label>Password</label><br/>
-            <input onChange = {handleChange}  name = "Password" type = "password" placeholder = "password"/><br/>
-            <p className = "ers">{errors.Password}</p>
-            <label>Re-Password</label><br/>
-            <input onChange = {handleChange} name = "Re_Password" type = "password" placeholder = "Re-Enter password"/><br/>
-            <p className = "ers">{errors.Re_Password}</p>
+            <input onChange = {handleChange}  name = "password" type = "password" placeholder = "password"/><br/>
+            <p className = "ers">{errors.password}</p>
+           
           </form>
           <Button  onClick = {handleSubmit} style = {{ marginTop : "10px" , width :"250px"}} variant="contained" color="primary" disableElevation>
       create Account
